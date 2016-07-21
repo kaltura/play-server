0b8b55660b828185b363498d7c9fa53b18b82631
root <nadav.harnik@kaltura.com>
2016-07-21 17:29:28 +0300

    fixed tests and modified clientConnector PLAT 5679 #time 0.5d

--- tests/testFlowAdTranscode.js
+++ tests/testFlowAdTranscode.js
@@ -8,10 +8,10 @@ const KalturaFFMpegCmdGenerator = require('../lib/utils/KalturaFFMpegCmdGenerato
 const KalturaMediaInfoResponse = require('../lib/utils/KalturaMediaInfoResponse');
 const TranscodingEngineResponse = require('../lib/infra/TranscodingEngineResponse');
 
-const serviceUrl = KalturaConfig.config.testClient.serviceUrl;
-const secret = KalturaConfig.config.testClient.secret;
-const partnerId = KalturaConfig.config.testClient.partnerId;
-const flavorId = KalturaConfig.config.testClient.flavorId;
+const serviceUrl = KalturaConfig.config.testing.serviceUrl;
+const secret = KalturaConfig.config.testing.secret;
+const partnerId = KalturaConfig.config.testing.partnerId;
+const flavorId = KalturaConfig.config.testing.flavorId;
 const filePath = `${__dirname}/resources/adSample`;
 const outPath = `${__dirname}/resources/adSample_output.mpg`;
 const info = new KalturaMediaInfo('ffprobe');
@@ -20,6 +20,7 @@ let response = null;
 let commandLine = null;
 
 describe('test the flow of ad transcode', function () {
+	this.timeout(0);
 	it('test - get mediaInfo for ad', function () {
 		return info.mediaInfoExec(filePath).then(function (data) {
 			expect(data).to.be.an.instanceof(KalturaMediaInfoResponse);
@@ -30,17 +31,8 @@ describe('test the flow of ad transcode', function () {
 		});
 	});
 
-	it('test - start client session', function() {
-		return connector.startSession().then(function (data) {
-			expect(data).to.not.be.null;
-			expect(connector.client.getKs()).to.not.be.null;
-		}, function (err) {
-			expect(err).to.be.null;
-		});
-	});
-
 	it('test - get command line via Api call', function () {
-		return KalturaFFMpegCmdGenerator.generateCommandLineFormat(flavorId, response.jsonInfo, connector).then(function (data) {
+		return KalturaFFMpegCmdGenerator.generateCommandLineFormat(flavorId, response.jsonInfo, connector, KalturaConfig.config.testing.partnerImp).then(function (data) {
 			expect(data).to.not.be.null;
 			commandLine = KalturaFFMpegCmdGenerator.fillCmdLineFormat(data, filePath, outPath);
 			expect(commandLine).to.not.be.null;
@@ -53,7 +45,7 @@ describe('test the flow of ad transcode', function () {
 		const engine = new TrancodinfEngine('ffmpeg');
 		return engine.transcodeFile(commandLine, filePath, outPath).then(function (data) {
 			expect(data).to.be.an.instanceof(TranscodingEngineResponse);
-			expect(data.ffmpegResponse.substring(0, 66)).to.equal('ffmpeg version 2.7.2 Copyright (c) 2000-2015 the FFmpeg developers');
+			expect(data.transcoderResponse.substring(0, 66)).to.equal('ffmpeg version 2.7.2 Copyright (c) 2000-2015 the FFmpeg developers');
 		}, function (err) {
 			expect(err).to.be.null;
 		});
