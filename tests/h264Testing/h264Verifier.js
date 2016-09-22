c5c94c4ea5740c363397e29f0469783eb874ac07
root <nadav.harnik@kaltura.com>
2016-09-22 16:42:08 +0300

    move test output to single log + calling the verification inside transcodingEngine PLAT-6031

--- tests/h264Testing/h264Verifier.js
+++ tests/h264Testing/h264Verifier.js
@@ -4,73 +4,118 @@ const fs = require('fs');
 require('../../lib/utils/KalturaConfig');
 require('../../lib/utils/KalturaCache');
 
+/**
+ * this class is used to verify that mp4s that compose a single flavor has the same h264 values
+ */
 class h264Verifier
 {
 	/**
+	 * inserts the flavor ids and flavor paths to the cache
 	 * @param flavorIds - array of flavor ids
 	 * @param flavorPaths - array of flavor paths
 	 */
 	static insertFlavorsPathToCache(flavorIds, flavorPaths)
 	{
-		h264Verifier.writeToLog(`called insertFlavorsPathToCache with ${flavorIds} and ${flavorPaths}`, 'h264.log');
+		h264Verifier.writeToLog(`called insertFlavorsPathToCache with ${flavorIds} and ${flavorPaths}`);
 		const testTime = KalturaConfig.config.h264Verfication.cacheTime;
 		for (let i = 0; i < flavorIds.length; i++)
 		{
 			KalturaCache.set(flavorIds[i], { path: flavorPaths[i] }, testTime,
 				function () {
-					h264Verifier.writeToLog(`inserted flavorId ${flavorIds[i]} with path ${flavorPaths[i]} to cache`, 'h264.log');
+					h264Verifier.writeToLog(`inserted flavorId ${flavorIds[i]} with path ${flavorPaths[i]} to cache`);
 				},
 				function () {
-					h264Verifier.writeToLog(`unable to insert flavorId ${flavorIds[i]} with path ${flavorPaths[i]} to cache`, 'h264.log');
+					h264Verifier.writeToLog(`unable to insert flavorId ${flavorIds[i]} with path ${flavorPaths[i]} to cache`);
 				}, null);
 		}
 	}
 
+	/**
+	 * calls the verification function with the flavorPath and the adPath
+	 * @param flavorId
+	 * @param adPath
+	 */
 	static verifyAd(flavorId, adPath)
 	{
-		h264Verifier.writeToLog(`called verifyAd with ${flavorId} and ${adPath}`, 'h264.log');
+		h264Verifier.writeToLog(`called verifyAd with ${flavorId} and ${adPath}`);
 		KalturaCache.get(flavorId,
 			function (flavorPath) {
-				h264Verifier.writeToLog(`got flavorId ${flavorId} from cache`, 'h264.log');
+				h264Verifier.writeToLog(`got flavorId ${flavorId} from cache`);
 				h264Verifier.runVerification(flavorId, flavorPath.path, adPath);
 			},
 			function (err) {
-				h264Verifier.writeToLog(`faild to get flavorId ${flavorId} from cache`, 'h264.log');
-				h264Verifier.writeToLog(`got err - ${err}`, 'h264.log');
+				h264Verifier.writeToLog(`faild to get flavorId ${flavorId} from cache`);
+				h264Verifier.writeToLog(`got err - ${err}`);
 			});
 	}
 
 	/**
-	 * runs verification on flavor and ad
+	 * runs the script on the flavor and the ad and calls verifyScriptOutput function on the results
 	 * @param flavorId
 	 * @param flavorObj
 	 * @param adPath
 	 */
 	static runVerification(flavorId, flavorPath, adPath)
 	{
-		h264Verifier.writeToLog(`called runVerification with flavorId - ${flavorId} , flavorPath - ${flavorPath} , adPath - ${adPath}`, 'h264.log');
+		h264Verifier.writeToLog(`called runVerification with flavorId - ${flavorId} , flavorPath - ${flavorPath} , adPath - ${adPath}`);
 		const scriptsPath = KalturaConfig.config.h264Verfication.scriptsPath;
 
 		h264Verifier.execPythonScript(`${scriptsPath}get_stsd_atoms.py ${flavorPath}`).then(function (flavorOutput) {
-			h264Verifier.writeToLog(`--------[${flavorId}][flavor]--------------`, `${flavorId}.log`);
-			h264Verifier.writeToLog(`${flavorOutput}`, `${flavorId}.log`);
-			h264Verifier.writeToLog(`--------[${flavorId}][flavor]--------------`, `${flavorId}.log`);
-
 			h264Verifier.execPythonScript(`${scriptsPath}get_stsd_atoms.py ${adPath}`).then(function (adOutput) {
-				h264Verifier.writeToLog(`--------[ad][${adPath}]--------------`, `${flavorId}.log`);
-				h264Verifier.writeToLog(`${adOutput}`, `${flavorId}.log`);
-				h264Verifier.writeToLog(`--------[ad][${adPath}]--------------`, `${flavorId}.log`);
+				h264Verifier.verifyScriptOutput(flavorId, adPath, flavorOutput, adOutput);
 			}, function (adErr) {
-				h264Verifier.writeToLog(`[error][flavorId][adPath][${adPath}] - ${adErr}`, `${flavorId}.log`);
+				h264Verifier.writeToLog(`[error][flavorId][adPath][${adPath}] - ${adErr}`);
 			});
 		}, function (flavorErr) {
-			h264Verifier.writeToLog(`[error][flavorId][${flavorId}] - ${flavorErr}`, `${flavorId}.log`);
+			h264Verifier.writeToLog(`[error][flavorId][${flavorId}] - ${flavorErr}`);
 		});
 	}
 
+	/**
+	 * verify the results
+	 * @param flavorId
+	 * @param adPath
+	 * @param flavorOutput
+	 * @param adOutput
+	 */
+	static verifyScriptOutput(flavorId, adPath, flavorOutput, adOutput)
+	{
+		const flavorArray = flavorOutput.split('\n');
+		const videoResultArray = [];
+		const audioResultArray = [];
+		for (let i = 0; i < flavorArray.length; i++)
+		{
+			if (flavorArray[i] === 'vide')
+				videoResultArray[flavorArray[i]] = i;
+			if (flavorArray[i] === 'soun')
+				audioResultArray[flavorArray[i]] = i;
+		}
+		const adArray = adOutput.split('\n');
+		for (let i = 0; i < adArray.length; i++)
+		{
+			if (adArray[i] === 'vide')
+				videoResultArray[adArray[i]] = i;
+			if (adArray[i] === 'soun')
+				audioResultArray[adArray[i]] = i;
+		}
+		if (videoResultArray.length !== 1 || audioResultArray.length !== 1)
+		{
+			let errorMsg = `*****[error] verification of flavor ${flavorId} and ad ${adPath} failed *****\n`;
+			errorMsg += `[flavorId ${flavorId} output]\n${flavorOutput}\n[ad ${adPath} output]\n${adOutput}\n`;
+			errorMsg += `****[error - end][${flavorId}][${adPath}]******`;
+			h264Verifier.writeToLog(errorMsg);
+		}
+		else
+			h264Verifier.writeToLog(`[success] verification of flavor ${flavorId} and ad ${adPath} success`);
+	}
+
+	/**
+	 * function to exec python scripts
+	 * @param commandLine
+	 */
 	static execPythonScript(commandLine)
 	{
-		h264Verifier.writeToLog(`called execPythonScript with ${commandLine}`, 'h264.log');
+		h264Verifier.writeToLog(`called execPythonScript with ${commandLine}`);
 		return new Promise(function (resolve, reject)
 		{
 			const runCommandLine = `python  ${commandLine}`;
@@ -87,9 +132,14 @@ class h264Verifier
 		});
 	}
 
-	static writeToLog(str, logFileName)
+	/**
+	 * writes str to a logFile, the logFilePath is defined in the config and must exist
+	 * @param str
+	 * @param logFileName
+	 */
+	static writeToLog(str)
 	{
-		fs.appendFile(`${KalturaConfig.config.h264Verfication.logPath}${logFileName}`, `${str}\n`);
+		fs.appendFile(`${KalturaConfig.config.h264Verfication.logPath}`, `${str}\n`);
 	}
 }
 module.exports = h264Verifier;
