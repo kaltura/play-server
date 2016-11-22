a2685a2f5c354b294859c9b26e093e6075b65b1b
root <nadav.harnik@kaltura.com>
2016-11-23 00:47:18 +0200

    tests fix

--- tests/unitTests/TestKalturaMediaInfo.js
+++ tests/unitTests/TestKalturaMediaInfo.js
@@ -13,6 +13,7 @@ const resourcesPath = KalturaConfig.config.testing.resourcesPath;
 const testsDirName = __dirname;
 const info = new KalturaMediaInfo('ffprobe');
 const badInfo = new KalturaMediaInfo('ffrobb');
+const continuationLocalStorage = require('continuation-local-storage');
 
 function removeWhiteSpaces(text){
 	return text.replace(/ /g,'');
@@ -30,41 +31,52 @@ function isEmpty(object)
 	return true;
 }
 
-
 describe('test KalturaMediaInfo class', function () {
-	it('test KalturaMediaInfo - get media info for a file that exist', function () {
+	this.timeout(0);
+	before(function() {
+		const namespace = continuationLocalStorage.createNamespace('play-server');
+	});
+	it('test KalturaMediaInfo - get media info for a file that exist', function (done) {
 		return info.mediaInfoExec(resourcesPath + '/adSample').then(function (data) {
 			expect(data).to.be.an.instanceof(KalturaMediaInfoResponse);
 			expect(removeWhiteSpaces(data.jsonInfo).substring(0, 20)).to.equal('{"programs":[],"stre');
+			done();
 		}, function (err) {
 			expect(err).to.be.null;
+			done();
 		});
 	});
 
-	it('test KalturaMediaInfo - file doesn\'t exist', function () {
+	it('test KalturaMediaInfo - file doesn\'t exist', function (done) {
 		const fileName = resourcesPath + '/12345';
 		return info.mediaInfoExec(fileName).then(function (data) {
 			expect(data).to.be.null;
+			done();
 		}, function (err) {
 			expect(err).to.equal(`File [${fileName}] doesn't exists on the file system`);
+			done();
 		});
 	});
 
-	it('test KalturaMediaInfo - command line error', function () {
+	it('test KalturaMediaInfo - command line error', function (done) {
 		return badInfo.mediaInfoExec(resourcesPath + '/adSample').then(function (data) {
 			expect(data).to.be.null;
+			done();
 		}, function (err) {
 			expect(err).to.be.an('error');
+			done();
 		});
 	});
 
-	it('test KalturaMediaInfo - emptyFile', function () {
+	it('test KalturaMediaInfo - emptyFile', function (done) {
 		return info.mediaInfoExec(resourcesPath + '/emptyFile').then(function (data) {
 			const parsedResult = JSON.parse(data.jsonInfo);
 			expect(isEmpty(parsedResult)).to.be.true;
+			done();
 		}, function (err) {
 			expect(err).to.not.be.null;
 			expect(err.message.substring(0, 25)).to.equal('Command failed: ffprobe -');
+			done();
 		});
 	});
 });
