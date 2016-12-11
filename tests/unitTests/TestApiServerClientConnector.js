0b4afa4825b57c6648d41f8d03f3aca696301f35
EladCohenKaltura <elad.cohen@kaltura.com>
2016-12-11 15:15:26 +0200

    Fix Test asynchronious calls

--- tests/unitTests/TestApiServerClientConnector.js
+++ tests/unitTests/TestApiServerClientConnector.js
@@ -21,9 +21,10 @@ describe('testApiClientConnector', function () {
 		const namespace = continuationLocalStorage.createNamespace('play-server');//Here just to make sure we create it only once
 		clsBluebird(namespace);
 	});
-	it('test session start', function () {
+	it('test session start', function (done) {
 		return connector._startSession().then(function (data) {
 			expect(data).to.not.be.null;
+			done();
 		}, function (err) {
 			if (!err)
 				expect(true).to.equal.true; // because it mean err == null
@@ -33,13 +34,15 @@ describe('testApiClientConnector', function () {
 				expect(err.response).to.not.equal('');
 				// because this mean we had cache fault but not actual fault
 			}
+			done();
 		});
 	});
-	it('test api exception', function () {
+	it('test api exception', function (done) {
 		let wrongSecret = '123456789abcdefghi';
 		const falseConnector = new ApiClientConnector(partnerId, wrongSecret, kalturaTypes.KalturaSessionType.ADMIN, serviceUrl);
 		return falseConnector._startSession().then(function (data) {
 			expect(data).to.be.null;
+			done();
 		}, function (err) {
 			if (typeof(err) == 'string')
 				expect(err).to.equal('KalturaAPIException Error while starting session for partner [-6]');
@@ -47,12 +50,14 @@ describe('testApiClientConnector', function () {
 				expect(err.objectType).to.equal('KalturaAPIException');
 				expect(err.message).to.equal('Error while starting session for partner [-6]');
 			}
+			done();
 		});
 	});
 
-	it('test handleApiRequest with uiConf get action ', function () {
+	it('test handleApiRequest with uiConf get action ', function (done) {
 		return connector.handleApiRequest('uiConf', 'get', [uiConfId], impersonatePartnerId).then(function (data) {
 			expect(data).to.have.property('objectType').and.equal('KalturaUiConf');
+			done();
 		}, function (err) {
 			if (!err)
 				expect(true).to.equal.true; // because it mean err == null
@@ -61,36 +66,42 @@ describe('testApiClientConnector', function () {
 				expect(err.response).to.have.property('objectType').and.equal('KalturaUiConf');
 				// because this mean we had cache fault but not actual fault
 			}
+			done();
 		});
 	});
 
-	it('test setValueInCache  ', function () {
+	it('test setValueInCache  ', function (done) {
 		const params = { apiCallService: 'someService', apiCallAction: 'someAction', params: [199], impersonatePartnerId: 101 };
 		const res = { member1: 'val1', member2: 'val2' };
 		return ApiClientConnector._setValueInCache({ cacheParams: params, response: res }).then(function (data) {
 			expect(data.response).to.have.property('member1').and.equal('val1');
 			expect(data.response).to.have.property('member2').and.equal('val2');
+			done();
 		}, function (err) {
 			expect(err).to.be.null;
+			done();
 		});
 	});
 
-	it('test getValueFromCache  ', function () {
+	it('test getValueFromCache  ', function (done) {
 		const params = { apiCallService: 'someService', apiCallAction: 'someAction', params: [199], impersonatePartnerId: 101};
 		return ApiClientConnector._getValueFromCache(params).then(
 			function (data) {
 				expect(data.response).to.have.property('member1').and.equal('val1');
 				expect(data.response).to.have.property('member2').and.equal('val2');
+				done();
 			}, function (err) {
 				expect(err).to.be.null;
+				done();
 			}
 		);
 	});
 
 
-	it('test handleApiRequest with flavorId get action', function () {
+	it('test handleApiRequest with flavorId get action', function (done) {
 		return connector.handleApiRequest('flavorAsset', 'get', [flavorId], impersonatePartnerId).then(function (data) {
 			expect(data).not.to.be.null;
+			done();
 		}, function (err) {
 			if (!err)
 				expect(true).to.equal.true; // because it mean err == null
@@ -99,6 +110,7 @@ describe('testApiClientConnector', function () {
 				expect(err.response).to.have.property('objectType').and.equal('KalturaFlavorAsset');
 				// because this mean we had cache fault but not actual fault
 			}
+			done();
 		});
 	});
 });
