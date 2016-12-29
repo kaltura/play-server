62fccec08e9e57789f245b3a90eaf11eaa29becd
EladCohenKaltura <elad.cohen@kaltura.com>
2016-12-29 15:01:04 +0200

    test fix

--- tests/unitTests/TestTranscodingEngine.js
+++ tests/unitTests/TestTranscodingEngine.js
@@ -5,27 +5,29 @@
 const chai = require('chai');
 const expect = chai.expect;
 const TranscodingEngineResponse = require('../../lib/infra/TranscodingEngineResponse');
-const TrancodinfEngine = require('../../lib/infra/TranscodingEngine');
+const KalturaTempFileHandler = require('../../lib/utils/KalturaTempFileHandler');
+const TranscodingEngine = require('../../lib/infra/TranscodingEngine');
 const path = require('path');
 const testDirName = __dirname;
 
 require('../../lib/utils/KalturaConfig');
 const fileName = KalturaConfig.config.testing.resourcesPath + '/adSample';
 const outputPath = KalturaConfig.config.testing.outputPath + '/adSample_output.mp4';
+const tempOutputPath = KalturaTempFileHandler._getTempFilePath(outputPath);
 const flavorId = '0_123456';
 require('../../lib/utils/KalturaLogger');
 
-const engine = new TrancodinfEngine('ffmpeg');
-const commandLine = ` -i ${fileName} -f mp4 -y ${outputPath}`;
+const engine = new TranscodingEngine('ffmpeg');
+const commandLine = ` -i ${fileName} -f mp4 -y ${tempOutputPath}`;
 const continuationLocalStorage = require('continuation-local-storage');
-let namespace = continuationLocalStorage.getNamespace('play-server');
+const namespace = continuationLocalStorage.getNamespace('play-server');
 if (!namespace)
 	continuationLocalStorage.createNamespace('play-server');
 
 describe('test TranscodeingEngine class', function () {
 	this.timeout(0);
 	it('test TranscodeingEngine - validate files path exists', function () {
-		return TrancodinfEngine.validateFilePaths(fileName, outputPath).then(function (data) {
+		return TranscodingEngine.validateFilePaths(fileName, outputPath).then(function (data) {
 			expect(data).to.be.true;
 		}, function (err) {
 			expect(err).to.be.null;
@@ -44,7 +46,7 @@ describe('test TranscodeingEngine class', function () {
 
 	it('test TranscodeingEngine - source file doest exist', function () {
 		const badfileName = `${testDirName}/resources/12345`;
-		return TrancodinfEngine.validateFilePaths(badfileName, outputPath).then(function (data) {
+		return TranscodingEngine.validateFilePaths(badfileName, outputPath).then(function (data) {
 			expect(data).to.be.null;
 		}, function (err) {
 			expect(err).to.equal(`File [${badfileName}] doesn't exists on the file system`);
@@ -53,15 +55,15 @@ describe('test TranscodeingEngine class', function () {
 
 	it('test TranscodeingEngine - library doest exist in save path', function () {
 		const badoutputPath = `${testDirName}/resources/test/adSample_output.mpg`;
-		return TrancodinfEngine.validateFilePaths(fileName, badoutputPath).then(function (data) {
+		return TranscodingEngine.validateFilePaths(fileName, badoutputPath).then(function (data) {
 			expect(data).to.be.null;
 		}, function (err) {
 			expect(err).to.equal(`[${path.dirname(badoutputPath)}] doesn't exists on the file system`);
 		});
 	});
 
-	it('test TranscodeingEngine - command line error', function() {
-		const badengine = new TrancodinfEngine('ffmpegg');
+	it('test TranscodingEngine - command line error', function() {
+		const badengine = new TranscodingEngine('ffmpegg');
 		return badengine.transcodeFile(commandLine, flavorId, outputPath).then(function (data) {
 			expect(data).to.be.null;
 		}, function (err) {
