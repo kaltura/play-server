1f3b8bac12436791af0e1c4000b4185113765b87
EladCohenKaltura <elad.cohen@kaltura.com>
2016-12-29 15:36:47 +0200

    Two Additional test fixes

--- tests/unitTests/TestFlowAdTranscode.js
+++ tests/unitTests/TestFlowAdTranscode.js
@@ -9,7 +9,7 @@ const TrancodinfEngine = require('../../lib/infra/TranscodingEngine');
 const KalturaFFMpegCmdGenerator = require('../../lib/utils/KalturaFFMpegCmdGenerator');
 const KalturaMediaInfoResponse = require('../../lib/utils/KalturaMediaInfoResponse');
 const TranscodingEngineResponse = require('../../lib/infra/TranscodingEngineResponse');
-
+const KalturaTempFileHandler = require('../../lib/utils/KalturaTempFileHandler');
 
 const serviceUrl = KalturaConfig.config.testing.serviceUrl;
 const secret = KalturaConfig.config.testing.secret;
@@ -17,6 +17,7 @@ const partnerId = KalturaConfig.config.testing.partnerId;
 const flavorId = KalturaConfig.config.testing.transcodedFlavorId;
 const filePath = KalturaConfig.config.testing.resourcesPath + '/adSample';
 const outPath = KalturaConfig.config.testing.outputPath + '/adSample_output.mpg';
+const tempOutputPath = KalturaTempFileHandler._getTempFilePath(outPath);
 const info = new KalturaMediaInfo('ffprobe');
 const connector = new ApiClientConnector(partnerId, secret, kalturaTypes.KalturaSessionType.ADMIN, serviceUrl);
 let response = null;
@@ -45,7 +46,7 @@ describe('test the flow of ad transcode', function () {
 		// 15 is the duration of /adSample
 		return KalturaFFMpegCmdGenerator.generateCommandLineFormat(flavorId, response.jsonInfo, 15, connector, KalturaConfig.config.testing.impersonatePartnerId).then(function (data) {
 			expect(data).to.not.be.null;
-			commandLine = KalturaFFMpegCmdGenerator.fillCmdLineFormat(data, filePath, outPath);
+			commandLine = KalturaFFMpegCmdGenerator.fillCmdLineFormat(data, filePath, tempOutputPath);
 			expect(commandLine).to.not.be.null;
 		}, function (err) {
 			expect(err).to.be.null;
